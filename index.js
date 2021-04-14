83352981c4418e5b6734e1eab35d1a27928c3be1
Wayne Rocha <dev.waynerocha@gmail.com>
2021-04-14 19:45:34 -0300

    :ok_hand: refactor
    function to get currencies upgraded with  a correct fetch, debug with console.table and Destructuring

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-let currencies;
-
 async function getCurrencies(){
-    const cr = await fetch('https://economia.awesomeapi.com.br/json/all')
-    .then(Response => Response.json())
-    .catch(() => (console.error(), getCurrencies()));
-    createCurrencyOptions(cr);
-    createTableOfValues(cr);
-    currencies = cr;
+    fetch('https://economia.awesomeapi.com.br/json/all')
+        .then(Response => Response.json().then(data => {
+            createCurrencyOptions(data);
+            logCurrencies(data);
+        }))
+        .catch(() => {
+            console.log('error to get currencies\ntrying again...');
+            getCurrencies();
+        });
 }
 function createCurrencyOptions(currencies){
     const selectElements = document.querySelectorAll('.currencyType');
-    for (const currency of Object.values(currencies)){
+    for (const {code} of Object.values(currencies)){
         selectElements.forEach(function(selectElement){
-            const tag = document.createElement('option');
-            tag.setAttribute('value', currency.code);
-            tag.innerText = currency.code;
+            const tag = document.createElement('option')
+            tag.setAttribute('value', code);
+            tag.innerText = code;
             selectElement.appendChild(tag);
         });
     }
 }
-
-function createTableOfValues(currencies){
-    //tabela temporaria - para fins de teste
-    let table = '<table cellpadding="0px" cellspacing="0px">';
-    table += '<tr><th>código</th><th>moeda</th><th>valor</th></tr>';
-    for (const currency of Object.values(currencies)){
-        table += '<tr>'
-        table += `<td>${currency.code}</td><td>${currency.name}</td><td>${currency.ask}</td></td>`
-        table += '</tr>'
-    }
-    table += '</table>';
-    document.body.innerHTML += table;
+function logCurrencies(currencies){
+    console.table(currencies);
 }
-
 getCurrencies();
